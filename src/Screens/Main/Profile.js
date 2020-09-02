e7de8845d3f3d5f2400c7cc76193a9ec30972c1a
ibrahim AKIN <ibrahim-akin@msn.com>
2020-09-02 20:32:06 +0300

    Add register page

--- src/Screens/Main/Profile.js
+++ src/Screens/Main/Profile.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Text, View } from 'react-native';
 
 const Profile = (props) => (
-    <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Profile</Text>
     </View>
 );
