a27d1832b50d2535d954ba66abb81ffe5bd01234
Emre <emre@yusuf-MacBook-Air.local>
2020-09-04 19:31:58 +0300

    initial commit

--- App.js
+++ App.js
@@ -8,30 +8,20 @@ import {
   Text,
   StatusBar,
 } from 'react-native';
-import Navigation from './src/Navigation/Navigation'
-
+import Router from './src/Router'
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
-
 import reducers from './src/Reducers'
 
 const App = () => {
   const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
   return (
     <Provider store={store}>
-      <Navigation />
+      <Router />
     </Provider>
 
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
-});
+  )
+}
 
 export default App;
---
+++ src/Router.js
@@ -0,0 +1,68 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import MainPage from './Screens/Main/MainPage'
+import Profile from './Screens/Main/Profile'
+import Stats from './Screens/Main/Stats'
+
+import FirstScreen from './Screens/Auth/FirstScreen'
+import Login from './Screens/Auth/Login'
+import Register from './Screens/Auth/Register'
+
+const AuthStack = createStackNavigator();
+
+const AuthStackScreen = () => {
+    return (
+        <AuthStack.Navigator initialRouteName='FirstScreen'>
+            <AuthStack.Screen
+                name="FirstScreen"
+                component={FirstScreen}
+                options={({ navigation, route }) => ({
+                    title: 'Login',
+                    headerShown: false
+                })}
+
+            />
+
+            <AuthStack.Screen
+                name="Login"
+                component={Login}
+                options={({ navigation, route }) => ({
+                    title: 'Login',
+                    headerShown: false
+                })}
+            />
+
+
+            <AuthStack.Screen
+                name="Register"
+                component={Register}
+                options={{
+                    title: 'Register',
+                    headerShown: false
+                }}
+            />
+
+        </AuthStack.Navigator>
+    )
+}
+
+const Tab = createBottomTabNavigator();
+
+const Navigation=() =>{
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Stats" component={Stats} />
+        <Tab.Screen name="MainPage" component={MainPage} />
+        <Tab.Screen name="Profile" component={Profile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Router;
